67a32995fd90d846ccbd096445b8b0c2b2befb0f
Gokul-anand-29 <gokulanand200@gmail.com>
2024-10-05 19:58:52 +0530

    Update Color_definition.h

--- Color_definition.h
+++ Color_definition.h
@@ -12,5 +12,7 @@ typedef struct {
 void ColorPairToString(const ColorPair* colorPair, char* buffer);
 ColorPair GetColorFromPairNumber(int pairNumber);
 int GetPairNumberFromColor(const ColorPair* colorPair);
+void testNumberToPair(int pairNumber, enum MajorColor expectedMajor,enum MinorColor expectedMinor);
+void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
 
 #endif //COLOR_DEFINITION_H
