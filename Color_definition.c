f7d447def76aa2c8c2e7fe207479071630d872a7
Gokul-anand-29 <gokulanand200@gmail.com>
2024-10-05 20:16:43 +0530

    Update Color_definition.c

--- Color_definition.c
+++ Color_definition.c
@@ -23,3 +23,19 @@ int GetPairNumberFromColor(const ColorPair* colorPair) {
     return colorPair->majorColor * numberOfMinorColors +
             colorPair->minorColor + 1;
 }
+
+void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor) {
+    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+    char colorPairNames[16];
+    ColorPairToString(&colorPair, colorPairNames);
+    printf("Got pair %s\n", colorPairNames);
+    assert(colorPair.majorColor == expectedMajor);
+    assert(colorPair.minorColor == expectedMinor);
+}
+
+void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber) {
+    ColorPair colorPair = {major, minor};
+    int pairNumber = GetPairNumberFromColor(&colorPair);
+    printf("Got pair number %d\n", pairNumber);
+    assert(pairNumber == expectedPairNumber);
+}
