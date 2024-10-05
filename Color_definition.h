cb6257ab0b5d8199d73fdffbc83fb4b24d4103ce
Gokul-anand-29 <gokulanand200@gmail.com>
2024-10-05 19:24:03 +0530

    Create Color_definition.h

---
+++ Color_definition.h
@@ -0,0 +1,7 @@
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
