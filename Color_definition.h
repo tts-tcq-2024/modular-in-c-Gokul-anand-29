d8070185121f9893a903f48366b4d9cfc7062a37
Gokul-anand-29 <gokulanand200@gmail.com>
2024-10-05 20:55:16 +0530

    Update Color_definition.h

--- Color_definition.h
+++ Color_definition.h
@@ -17,5 +17,6 @@ ColorPair GetColorFromPairNumber(int pairNumber);
 int GetPairNumberFromColor(const ColorPair* colorPair);
 void testNumberToPair(int pairNumber, enum MajorColor expectedMajor,enum MinorColor expectedMinor);
 void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
+void CodeReferenceManual(int numberOfMajorColors, int numberOfMinorColors);
 
 #endif //COLOR_DEFINITION_H
